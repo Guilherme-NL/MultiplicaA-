dfadff4aadfabdfab7758a3870fb10f7fe904518
Guilherme-NL <guilherme.nunes.lopes@gmail.com>
2023-01-11 16:41:57 -0300

    fix: remove \n and use println

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 public class App {
     public static void main(String[] args) {
         int num = 5;
-        System.out.print("Tabela de multiplicação do " + num + "\n");
+        System.out.println("Tabela de multiplicação do " + num);
         for (int x = 0; x <= 10; x++) {
          System.out.print("Tabuada do: ");
          System.out.print(x + " x " + num + " = ");
